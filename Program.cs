cf42b3fba373198d774c735f1ac0fa169da3906b
Ribas <yghor.dev@gmail.com>
2020-12-11 15:34:53 -0300

    Add C# neural

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Simple_NeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] w = {0.8233245475,1.1233256475,1.0533245475};
+            int input;
+            input = int.Parse(Console.ReadLine());
+            double output = 0;
+
+            for(int i = 0; i < w.Length; i++) {
+                output += input * w[i];
+            }
+            
+            Console.WriteLine($"Result: {output}");
+        }
+    }
+}
